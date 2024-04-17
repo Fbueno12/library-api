9be4e8849dd0b86f46a85d50597da4d50c1024ba
fbueno12 <felipebueno181@gmail.com>
2024-04-17 20:52:04 -0300

    generate BookModel n initial config

---
+++ src/src/test/kotlin/br/com/fbueno/libraryAPI/LibraryApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.fbueno.libraryAPI
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LibraryApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
